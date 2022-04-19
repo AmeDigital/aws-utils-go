ff8c57aab992584654e960ff0b17ca6302137ebd
Luiz Matos <luiz.matos@amedigital.com>
2022-04-19 17:38:28 -0300

    chore(build): adiciona script de build
    - adiciona script de build para instalacao e execucao de testes no projeto

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function error () {
+    message=$1
+    echo "ERROR: $message"
+    exit 1
+}
+
+folders=(cognitoutils dynamodbutils localstack s3utils sessionutils snsutils sqsutils)
+
+echo "Installing aws-utils-go..."
+
+for folder in "${folders[@]}"; do
+    cd "$folder" || error "$folder does not exists!"
+    echo -n "Installing $folder..."
+    go get -d -v || error "$folder installation failed!"
+    if [[ $folder == "localstack" ]] || [[ $folder == "dynamodbutils" ]]; then
+        echo -n " Testing $folder..."
+        go test || error "$folder tests failed!"
+    else
+        echo "👌"
+    fi
+    cd ..
+done
+
+echo "aws-utils-go successfully installed and tested"
