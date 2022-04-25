acc5e0cc773ecf30ab2380331a0280f33beeaf1c
Luiz Matos <luiz.matos@amedigital.com>
2022-04-25 10:52:14 -0300

    refactor(build): executa testes apenas se ambiente passado for local

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ function error () {
     exit 1
 }
 
+env=$1
+
 folders=(cognitoutils dynamodbutils localstack s3utils sessionutils snsutils sqsutils)
 
 echo "Installing aws-utils-go..."
@@ -14,13 +16,17 @@ for folder in "${folders[@]}"; do
     cd "$folder" || error "$folder does not exists!"
     echo -n "Installing $folder..."
     go get -d -v || error "$folder installation failed!"
-    if [[ $folder == "localstack" ]] || [[ $folder == "dynamodbutils" ]]; then
-        echo -n " Testing $folder..."
-        go test || error "$folder tests failed!"
+    if [[ $env == "local" ]]; then
+        if [[ $folder == "localstack" ]] || [[ $folder == "dynamodbutils" ]]; then
+            echo -n " Testing $folder..."
+            go test || error "$folder tests failed!"
+        else
+            echo "👌"
+        fi
     else
         echo "👌"
     fi
     cd ..
 done
 
-echo "aws-utils-go successfully installed and tested"
+echo "aws-utils-go successfully installed"
